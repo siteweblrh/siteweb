edded75be52be8469cd11a3b79f369d6bb29bb66
MikeRanai <mike.ranai@outlook.fr>
2026-05-12 08:32:03 +0400

    feat: initialisation stack Next.js, Tailwind v4, Prisma et charte LRH

---
+++ prisma/migrations/20260512041958_init_db/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Club" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
+);
